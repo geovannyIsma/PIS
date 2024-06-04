3e34eb997ab9d64cf305e509cad3af8cab13c62a
geovanny <geovanny.romero@unl.edu.ec>
2024-06-04 18:07:27 -0500

    Estructura del proyecto

--- PIS/settings.py
+++ PIS/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Prediccion.apps.PrediccionConfig',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ec'
 
 TIME_ZONE = 'UTC'
 
